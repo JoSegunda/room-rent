495ba0375e40c7dce3a92f12b6a8eff0afcdb3bd
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-07 15:38:18 +0000

    adicionar spring-boot project starter

---
+++ backend/src/test/java/com/roomrent/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomrent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
