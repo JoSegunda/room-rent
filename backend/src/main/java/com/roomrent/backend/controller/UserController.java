25de37fbf19cb0fdd1975509f1cfca02f124bcc5
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:59:50 +0000

    adicionar registo de user

--- backend/src/main/java/com/roomrent/backend/controller/UserController.java
+++ backend/src/main/java/com/roomrent/backend/controller/UserController.java
@@ -4,6 +4,18 @@ import com.roomrent.backend.model.User;
 import com.roomrent.backend.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
 }
