535757af13831ebe89cffd8f3e09f905d0aec4e1
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:59:14 +0000

    adicionar imports

--- backend/src/main/java/com/roomrent/backend/controller/UserController.java
+++ backend/src/main/java/com/roomrent/backend/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.roomrent.backend.controller;
 
+import com.roomrent.backend.model.User;
+import com.roomrent.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
 public class UserController {
     
 }
