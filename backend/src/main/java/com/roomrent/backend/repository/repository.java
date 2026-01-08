ea91d1a8cc820aa7ca82b9eef3e200b419936aed
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:50:32 +0000

    importar o User

--- backend/src/main/java/com/roomrent/backend/repository/repository.java
+++ backend/src/main/java/com/roomrent/backend/repository/repository.java
@@ -1,5 +1,8 @@
 package com.roomrent.backend.repository;
 
+import com.roomrent.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public class repository {
     
 }
