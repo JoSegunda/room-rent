89588791833e4893d668353670dac7cb905859c5
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 17:06:42 +0000

    ligar a base de dados com o front-end

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -3,3 +3,32 @@ fetch("http://localhost:8080/api/hello")
   .then(data => {
     console.log(data);
   });
+
+  //Register form
+
+  document.getElementById("registerForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const user = {
+    name: document.getElementById("fname").value + document.getElementById("lname").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
+  };
+
+  fetch("http://localhost:8080/api/users/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res => {
+    if (!res.ok) throw new Error("Erro ao registar");
+    return res.json();
+  })
+  .then(data => {
+    alert("Utilizador registado com sucesso!");
+    console.log(data);
+  })
+  .catch(err => alert(err.message));
+});
