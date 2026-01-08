769001b69ac3ac2e2bea48a9088642985aeaa7e9
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:58:20 +0000

    adicionar register ao service

--- backend/src/main/java/com/roomrent/backend/service/UserService.java
+++ backend/src/main/java/com/roomrent/backend/service/UserService.java
@@ -7,5 +7,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email já registado");
+        }
+        return userRepository.save(user);
+    }
 }
