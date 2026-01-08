1c750a15bd680ed3376e3e6cfbf8c5907c47bdbb
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:56:15 +0000

    adicionar imports ao service

---
+++ backend/src/main/java/com/roomrent/backend/service/UserService.java
@@ -0,0 +1,11 @@
+package com.roomrent.backend.service;
+
+import com.roomrent.backend.model.User;
+import com.roomrent.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+    
+}
