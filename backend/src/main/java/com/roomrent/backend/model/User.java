71238c30ff5d19d2f7dd38ad56f0c4374beceed4
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-08 16:45:46 +0000

    adicionar atributos aos users

--- backend/src/main/java/com/roomrent/backend/model/User.java
+++ backend/src/main/java/com/roomrent/backend/model/User.java
@@ -5,5 +5,14 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
 }
