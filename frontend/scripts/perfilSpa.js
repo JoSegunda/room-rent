4f10d66bbffd1884fea495f8e6ba03ee2870daea
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-15 12:51:55 +0000

    colocar form em ficheiro txt

--- frontend/scripts/perfilSpa.js
+++ frontend/scripts/perfilSpa.js
@@ -1,23 +1,33 @@
+  console.log("Working")
+
 const updateContent = () => {
   const content = document.getElementById('dinamic-container');
+  console.log("Working")
   const hash = window.location.hash.substring(1); // Remove the '#' from the hash
 
   switch (hash) {
-    case 'home':
+    case 'nav-perfil':
       content.innerHTML = `
           <h1>Home</h1>
           <p>Welcome to the home page.</p>
         `;
       break;
-    case 'about':
+    case 'nav-post':
+        const fileUrl = '../pages/add-post-form.txt';
+        fetch(fileUrl)
+            .then(r => r.text())
+            .then(t => content.innerHTML = t)
+      
+      break;
+    case 'nav-ads':
       content.innerHTML = `
-          <h1>About</h1>
-          <p>Learn more about us on this page.</p>
+          <h1>Meus anúncios</h1>
+          <p>Get in touch with us.</p>
         `;
       break;
-    case 'contact':
+    case 'nav-chat':
       content.innerHTML = `
-          <h1>Contact</h1>
+          <h1>conversas</h1>
           <p>Get in touch with us.</p>
         `;
       break;
