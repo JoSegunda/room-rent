191e453c6db9b2bd6dca938059c41b642fd7551c
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-15 16:02:25 +0000

    adicionar anuncio repository

--- backend/src/main/java/com/roomrent/backend/repository/AnuncioRepository.java
+++ backend/src/main/java/com/roomrent/backend/repository/AnuncioRepository.java
@@ -1,3 +1,7 @@
-public class AnuncioRepository {
-    
+package com.roomrent.backend.repository;
+
+import com.roomrent.backend.model.Anuncio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnuncioRepository extends JpaRepository<Anuncio, Long> {
 }
